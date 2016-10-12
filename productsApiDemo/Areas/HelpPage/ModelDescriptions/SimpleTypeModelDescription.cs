f8ba0985f5e33d59aaafaa87fe837cd205a6da5a
Ankit Bansal <Ankit Bansal@ANKIT-LAPPY>
2016-10-12 13:07:50 +0530

    Add project files.

---
+++ productsApiDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace productsApiDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ productsApiDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="productsApiDemo.WebApiApplication" Language="C#" %>
